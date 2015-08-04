803e425aea94122ca359931a8b6276a375258df8
Student <student@fanyaledeMac-mini.local>
2015-08-04 14:24:02 +0800

    Initial Commit

---
+++ firstC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  firstC
+//
+//  Created by Student on 15/8/4.
+//  Copyright (c) 2015年 yyt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
